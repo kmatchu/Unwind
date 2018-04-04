1826ed108139ce45f4ce82693854b61fb8482e8b
kmatchu <kevinwmathew@gmail.com>
2018-04-04 11:40:07 -0500

    talking to local DB

--- unwind/Solved/client/src/utils/API.js
+++ unwind/Solved/client/src/utils/API.js
@@ -14,7 +14,7 @@ export default {
     return axios.delete("/api/books/" + id);
   },
   // Saves a book to the database
-  saveBook: function(bookData) {
-    return axios.post("/api/books", bookData);
+  saveBook: function(userData) {
+    return axios.post("/api/books", userData);
   }
 };
